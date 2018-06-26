4ff928375cde1da0201d0f27de9775b28d662ccf
ceniza.sjs <ceniza.sjs@sprobe.com>
2018-06-26 10:50:14 +0800

    Implement checking of letter input.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ The main program for the mini-game
 import HangmanUtilities
 
 game_screen = HangmanUtilities.game_screen
+check_char = HangmanUtilities.check_char
 
 if __name__ == '__main__':
     guessCount = 0
@@ -20,19 +21,24 @@ if __name__ == '__main__':
     while(guessCount < 6):
         game_screen(hangmanIndex, guessedWord, guessedLetters)
         guessedLetter = input("Input a letter: ").upper()
-        flag = HangmanUtilities.check_letter(word, guessedWord, guessedLetter)
+        if (check_char(guessedLetter)):
+            flag = HangmanUtilities.check_letter(word, guessedWord, guessedLetter)
 
-        if (not flag):
-            if (guessedLetter not in guessedLetters):
-                guessedLetters.append(guessedLetter)
-                hangmanIndex = hangmanIndex + 1
-                guessCount = guessCount + 1
-        
-        if ('_' not in guessedWord):
-            game_screen(hangmanIndex, guessedWord, guessedLetters)
-            print("You guessed the word!")
-            winFlag = True
-            break
+            if (not flag):
+                if (guessedLetter not in guessedLetters):
+                    guessedLetters.append(guessedLetter)
+                    hangmanIndex = hangmanIndex + 1
+                    guessCount = guessCount + 1
+            
+            if ('_' not in guessedWord):
+                game_screen(hangmanIndex, guessedWord, guessedLetters)
+                print("You guessed the word!")
+                winFlag = True
+                break
+        else:
+            print("Error! Please enter a letter.")
+            print("Press any key to continue.")
+            input()
 
     if (not winFlag):
         game_screen(hangmanIndex, guessedWord, guessedLetters)
